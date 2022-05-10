1cdb2df2162edcef20789f956e16ebd4751870e6
bilal zaneen <bilal@aeliasoft.com>
2022-05-10 10:02:08 +0300

    fix nigative and zeros

--- app/Helper/Triangle.php
+++ app/Helper/Triangle.php
@@ -12,7 +12,9 @@ class Triangle{
     public function TriangleType ($side1,$side2,$side3)
     {
         $type="";
-        if($side1 == $side2 && $side2 == $side3){
+        if($side1 <=0 || $side2 <=0 || $side3 <=0  ){
+            $type = "invalid"; 
+        } elseif($side1 == $side2 && $side2 == $side3){
             $type="Isosceles"; 
         }elseif($side1 == $side2 || $side1 == $side3 || $side2 == $side3){
             $type="Equilateral"; 
